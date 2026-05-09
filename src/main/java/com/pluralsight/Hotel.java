a39c0f9a1e59b787a929859a4751f6339edde124
abdulmeliktuhaye <abdulmelikmohammednur123@gmail.com>
2026-05-09 07:41:49 -0500

    Third Commit

---
+++ src/main/java/com/pluralsight/Hotel.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+
+//This class represents a hotel
+public class Hotel{
+
+    //Stores the hotel name
+    private String name;
+
+    //Stores the total number of suites
+    private int numberOfSuites;
+
+    //Stores the total number of basic rooms
+    private int numberOfRooms;
+
+    //Stores how many suites are booked
+    private int bookedSuites;
+
+    //Stores how many basic rooms are booked
+    private int bokkedBasicRooms;
+}
